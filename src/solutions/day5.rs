03a4cae72717fee850837bffff73cf7079255c91
Sathya Tadinada <sathyav.tadinada@gmail.com>
2025-12-23 14:49:15 -0500

    Started day5, created day_template for future use

---
+++ src/solutions/day5.rs
@@ -0,0 +1,33 @@
+crate::day!("Cafeteria" => {
+    part_1,
+    part_2
+});
+
+fn part_1(data: &str) -> isize {
+    0
+}
+
+fn part_2(data: &str) -> isize {
+    0
+}
+
+crate::test_day!(
+
+"
+3-5
+10-14
+16-20
+12-18
+
+1
+5
+8
+11
+17
+32
+",
+{
+    part_1 => 3,
+    part_2 => 0
+}
+);
